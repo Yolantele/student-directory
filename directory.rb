cb2cfcb446bf9b3a6538cf5c9b6446aa245b3802
Jolanta Jas <jolantajas@Jolantas-MacBook-Air.local>
2017-10-13 20:52:48 +0100

    interactive menu , with input, display and exit fnctions

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+@students = [] # an empty array accessible accross all methods.
+
 def rand_month
   months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" ]
   month = months[rand(months.length)]
@@ -6,16 +8,15 @@ end
 def input_students
   puts "please enter the names, previous careers and years of age of the students"
   puts "To finish,just hit return three times"
-  students = []
   name = gets.chomp
   career = gets.chomp
   years = gets.chomp
   if !name.empty?
-    students << {name: name, cohort: rand_month, prev_career: career, years_old: years}
-    if students.count < 2
-      puts "Now we have #{students.count} student"
-    else students.count > 2
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: rand_month, prev_career: career, years_old: years}
+    if @students.count < 2
+      puts "Now we have #{@students.count} student"
+    else @students.count > 2
+    puts "Now we have #{@students.count} students"
     end
   elsif name.empty?
   puts "You haven't entered any students"
@@ -25,7 +26,6 @@ def input_students
     career = gets.chomp
     years = gets.chomp
   end
-  students
   end
 
 def print_header
@@ -33,24 +33,53 @@ def print_header
   puts "----------------"
 end
 
-def prints(students)
-  if students.count == 0
+def print_students_list
+  if @students.count == 0
     puts "...No student entries"
   else
-  students.each_with_index do |student, index|
+  @students.each_with_index do |student, index|
     puts "#{index +=1}. #{student[:name]} (#{student[:cohort]} cohort), previously worked as #{student[:prev_career]}, but decided to change to career in coding at the age of #{student[:years_old]}"
   end
  end
 end
 
-def print_footer(names) # we print the total number of students here
-unless names.count == 0 then
-  puts "Overall, we have #{names.count} great students"
+def print_footer # we print the total number of students here
+unless @students.count == 0 then
+  puts "Overall, we have #{@students.count} great students"
   end
 end
 
-#calling the methods to activate their function
-students = input_students
-print_header
-prints(students)
-print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the student"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+   input_students
+  when "2"
+   show_students
+  when "9"
+   exit
+  else
+   puts "I didn't uderstand what you asked for, please try again."
+  end
+ end
+
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
