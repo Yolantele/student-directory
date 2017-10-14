38f4592313f1af4f893a43b61f7fb1fef3c742ad
Jolanta Jas <jolantajas@Jolantas-MacBook-Air.local>
2017-10-14 09:28:44 +0100

    file loading function added to the menu

--- directory.rb
+++ directory.rb
@@ -53,6 +53,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the student"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -68,15 +69,16 @@ def process(selection)
    input_students
   when "2"
    show_students
- when "3"
+  when "3"
    save_students
+  when "4"
+   load_students   
   when "9"
-   exit
+   exit #terminates program
   else
    puts "I didn't uderstand what you asked for, please try again."
   end
  end
-
 
 def interactive_menu
   loop do
@@ -84,6 +86,7 @@ def interactive_menu
     process(gets.chomp)
   end
 end
+
 
 def save_students
   # open the file for writing
@@ -93,6 +96,15 @@ def save_students
     student_data = [student[:name], student[:cohort], student[:prev_career], student[:years_old]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort, career, years = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym, prev_career: career, years_old: years}
   end
   file.close
 end
